1416bf0a2b6b3b6a62c3a390da6d6c96bfdff426
Lee Cheng Wei <aeggli@gmail.com>
2018-08-30 19:47:51 +0800

    Fix #6

--- src/test/util/test_nodetool.py
+++ src/test/util/test_nodetool.py
@@ -102,13 +102,13 @@ class TestNodeTool(MockedClientTest):
 
     @patch('cassandras3.util.nodetool.sh')
     def test_ensure_dir(self, mock_sh):
-        self.nodetool._ensure_dir('table')
+        self.nodetool._ensure_dir('keyspace', 'table')
         mock_sh.mkdir.assert_called_with('-p', '%s/%s' % ('/var/lib/cassandra/data', 'table'))
 
     @patch('cassandras3.util.nodetool.sh')
     def test_ensure_dir_exception(self, mock_sh):
         mock_sh.mkdir.side_effect = Exception('kaboom')
-        self.nodetool._ensure_dir('table')
+        self.nodetool._ensure_dir('keyspace', ''table')
 
     @patch('cassandras3.util.nodetool.sh')
     def test_lookup_snapshots(self, mock_sh):
