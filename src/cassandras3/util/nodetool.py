c69d5be63c007be51160a2e488b1201fd55fc757
Sumit Murari <sumit@murari.me>
2019-06-22 20:28:28 +0530

    Added few debug messages

--- src/cassandras3/util/nodetool.py
+++ src/cassandras3/util/nodetool.py
@@ -35,7 +35,6 @@ class NodeTool(object):
         logger.debug('s3 path is %s', s3_path)
 
         self._snapshot(keyspace, tag)
-
         for cass_dir in self._lookup_snapshots(tag):
             for root, _, files in os.walk(cass_dir):
                 for filename in files:
@@ -44,7 +43,6 @@ class NodeTool(object):
                     local_path = os.path.join(root, filename)
                     self._upload_file(
                         local_path, bucket, s3_path, table, filename)
-
         print('Successfully backed up your cassandra keyspace with backup' +
               ' ID "%s"' % timestamp)
 
@@ -105,15 +103,14 @@ class NodeTool(object):
         return extra_args
 
     def _upload_file(self, local_path, bucket, s3_path, table, filename):
+        logger.debug('Uploading file {}/{}/{}'.format(s3_path, table, filename))
         try:
             self.s3.upload_file(local_path,
-                                bucket,
-                                '%s/%s/%s' % (s3_path, table, filename),
-                                ExtraArgs=self._s3_extra_args())
-        except ClientError as e:
-            logger.error(e)
-            return False
-        return True
+                    bucket,
+                    '%s/%s/%s' % (s3_path, table, filename),
+                    ExtraArgs=self._s3_extra_args())
+        except:
+            logger.error('Failed in uploading file')
 
     def _download_file(self, bucket, filename, keyspace, table):
         key = filename.split('/')[-1]
@@ -123,16 +120,19 @@ class NodeTool(object):
                 ExtraArgs=self._s3_extra_args())
 
     def _ensure_dir(self, keyspace, table):
+        logger.debug('Ensuring directory {} exists'.format(self.cassandra_data_dir + keyspace + table + key))
         try:
             sh.mkdir('-p', '%s/%s/%s' % (self.cassandra_data_dir, keyspace, table))
         except:
             logger.warning('Could not create directory!')
 
     def _lookup_snapshots(self, tag):
+        logger.debug('Searching for snapshots with tag: {} '.format(tag))
         try:
             dirs = sh.find(self.cassandra_data_dir,
                            '-name',
                            tag)
+            logger.debug('Found directory: {}'.format(dirs))
         except:
             logger.warning('Unable to execute find, nodetool did not create snapshot?')
             dirs = ''
@@ -140,30 +140,34 @@ class NodeTool(object):
         return dirs.splitlines()
 
     def _snapshot(self, keyspace, tag):
+        logger.debug('Taking snapshot for {0} keyspace with tag: {1}'.format(keyspace, tag))
         try:
             if self.jmxusername and self.jmxpassword:
-                sh.nodetool('-u', self.jmxusername, '-pw', self.jmxpassword,
+                output = sh.nodetool('-u', self.jmxusername, '-pw', self.jmxpassword,
                             '-h', self.host, '-p', self.port, 'snapshot', '-t', tag, keyspace)
             else:
-                sh.nodetool('-h', self.host, '-p', self.port, 'snapshot', '-t', tag, keyspace)
+                output = sh.nodetool('-h', self.host, '-p', self.port, 'snapshot', '-t', tag, keyspace)
+
+            logger.error('Snapshot returned with status code {}'.format(output.exit_code))
         except:
             logger.error('Command possibly unfinished due to errors!')
             raise
 
     def _clearsnapshot(self, keyspace, tag):
         try:
+            logger.debug('Clearing snapshots for {} keyspace and {} tag'.format(keyspace, tag))
             if self.jmxusername and self.jmxpassword:
                 sh.nodetool('-u', self.jmxusername, '-pw', self.jmxpassword,
                             '-h', self.host, '-p', self.port, 'clearsnapshot', '-t', tag, keyspace)
             else:
                 sh.nodetool('-h', self.host, '-p', self.port, 'clearsnapshot', '-t', tag, keyspace)
-
         except:
             logger.error('Command possibly unfinished due to errors!')
             raise
 
     def _refresh(self, keyspace, table):
         try:
+            logger.debug('Refreshing: nodetool refresh')
             if self.jmxusername and self.jmxpassword:
                 sh.nodetool('-u', self.jmxusername, '-pw', self.jmxpassword,
                             '-h', self.host, '-p', self.port, 'refresh', keyspace, table)
