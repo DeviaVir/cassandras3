3ad720291463b26222e10071e07c3baae500fa14
Adam Sowiński <adassow@gmail.com>
2019-07-11 13:01:25 +0200

    set log level for urllib3 (#15)

--- src/cassandras3/log.py
+++ src/cassandras3/log.py
@@ -7,6 +7,7 @@ def setup_logging(level=logging.DEBUG):  # pragma: no cover
     logging.basicConfig(level=logging.DEBUG,
                         format='%(asctime)s - %(levelname)s - %(message)s')
     logging.getLogger('boto3').setLevel(logging.CRITICAL)
+    logging.getLogger('urllib3').setLevel(logging.CRITICAL)
     logging.getLogger('botocore').setLevel(logging.CRITICAL)
     logging.getLogger('s3transfer').setLevel(logging.CRITICAL)
     logging.getLogger('sh').setLevel(logging.CRITICAL)
